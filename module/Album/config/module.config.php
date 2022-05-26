bdb1404b364494078877d4bf3d1d7dd027446ae9
mark <markpackham1@gmail.com>
2022-05-26 15:35:21 +0100

    apply routes
    https://docs.laminas.dev/tutorials/getting-started/routing-and-controllers/

--- module/Album/config/module.config.php
+++ module/Album/config/module.config.php
@@ -2,6 +2,7 @@
 
 namespace Album;
 
+use Laminas\Router\Http\Segment;
 use Laminas\ServiceManager\Factory\InvokableFactory;
 
 return [
@@ -10,6 +11,26 @@ return [
             Controller\AlbumController::class => InvokableFactory::class,
         ],
     ],
+
+    'router' => [
+        'routes' => [
+            'album' => [
+                'type'    => Segment::class,
+                'options' => [
+                    'route' => '/album[/:action[/:id]]',
+                    'constraints' => [
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ],
+                    'defaults' => [
+                        'controller' => Controller\AlbumController::class,
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
+        ],
+    ],
+
     'view_manager' => [
         'template_path_stack' => [
             'album' => __DIR__ . '/../view',
