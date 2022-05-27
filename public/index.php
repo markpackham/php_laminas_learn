81537c2c4a940c0421491429d4888734bf2a5fc3
mark <markpackham1@gmail.com>
2022-05-27 09:12:00 +0100

    output more errors for debugging

--- public/index.php
+++ public/index.php
@@ -1,9 +1,15 @@
 <?php
 
-declare(strict_types=1);
-
 use Laminas\Mvc\Application;
 use Laminas\Stdlib\ArrayUtils;
+
+/**
+ * Display all errors when APPLICATION_ENV is development.
+ */
+if ($_SERVER['APPLICATION_ENV'] === 'development') {
+    error_reporting(E_ALL);
+    ini_set("display_errors", '1');
+}
 
 /**
  * This makes our life easier when dealing with paths. Everything is relative
@@ -14,7 +20,7 @@ chdir(dirname(__DIR__));
 // Decline static file requests back to the PHP built-in webserver
 if (php_sapi_name() === 'cli-server') {
     $path = realpath(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
-    if (is_string($path) && __FILE__ !== $path && is_file($path)) {
+    if (__FILE__ !== $path && is_file($path)) {
         return false;
     }
     unset($path);
@@ -23,12 +29,12 @@ if (php_sapi_name() === 'cli-server') {
 // Composer autoloading
 include __DIR__ . '/../vendor/autoload.php';
 
-if (! class_exists(Application::class)) {
+if (!class_exists(Application::class)) {
     throw new RuntimeException(
         "Unable to load application.\n"
-        . "- Type `composer install` if you are developing locally.\n"
-        . "- Type `vagrant ssh -c 'composer install'` if you are using Vagrant.\n"
-        . "- Type `docker-compose run laminas composer install` if you are using Docker.\n"
+            . "- Type `composer install` if you are developing locally.\n"
+            . "- Type `vagrant ssh -c 'composer install'` if you are using Vagrant.\n"
+            . "- Type `docker-compose run laminas composer install` if you are using Docker.\n"
     );
 }
 
