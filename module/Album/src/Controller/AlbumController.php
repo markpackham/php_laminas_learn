226309f5f843ebd498fa871a8e552697a81de566
mark <markpackham1@gmail.com>
2022-05-27 08:52:04 +0100

    add view for index

--- module/Album/src/Controller/AlbumController.php
+++ module/Album/src/Controller/AlbumController.php
@@ -2,8 +2,9 @@
 
 namespace Album\Controller;
 
-use Laminas\Mvc\Controller\AbstractActionController;
+use Album\Model\AlbumTable;
 use Laminas\View\Model\ViewModel;
+use Laminas\Mvc\Controller\AbstractActionController;
 
 class AlbumController extends AbstractActionController
 {
@@ -16,6 +17,9 @@ class AlbumController extends AbstractActionController
 
     public function indexAction()
     {
+        return new ViewModel([
+            'albums' => $this->table->fetchAll(),
+        ]);
     }
 
     public function addAction()
