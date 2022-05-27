c208bd336606919656b6759015d52a5f81cf9bc6
mark <markpackham1@gmail.com>
2022-05-27 08:44:24 +0100

    add dependancy for album table

--- module/Album/src/Controller/AlbumController.php
+++ module/Album/src/Controller/AlbumController.php
@@ -7,6 +7,13 @@ use Laminas\View\Model\ViewModel;
 
 class AlbumController extends AbstractActionController
 {
+    private $table;
+
+    public function __construct(AlbumTable $table)
+    {
+        $this->table = $table;
+    }
+
     public function indexAction()
     {
     }
