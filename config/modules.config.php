b3628614918b4be881ce1897e8637e6e66b8addc
mark <markpackham1@gmail.com>
2022-05-30 09:48:25 +0100

    fix laminas/laminas-inputfilter requirement

--- config/modules.config.php
+++ config/modules.config.php
@@ -6,6 +6,8 @@
  * This should be an array of module namespaces used in the application.
  */
 return [
+    'Laminas\InputFilter',
+    'Laminas\Filter',
     'Laminas\Db',
     'Laminas\Router',
     'Laminas\Validator',
